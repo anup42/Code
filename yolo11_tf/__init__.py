b5a64f201b87efde105db726127e912db40d7432
Anup <anupkushwaha@gmail.com>
2025-09-15 22:11:59 +0530

    chore(pkg): set TF_XLA_FLAGS=--tf_xla_auto_jit=0 at package import to avoid XLA UnsortedSegment GPU crash

--- yolo11_tf/__init__.py
+++ yolo11_tf/__init__.py
@@ -1,6 +1,11 @@
-__all__ = [
-    "build_yolo11",
-]
+"""Package init for YOLO11-TF utilities.
 
-from .model import build_yolo11
+Sets safe defaults to avoid known TensorFlow GPU crashes with
+UnsortedSegment kernels when XLA JIT is enabled via environment.
+"""
+
+import os as _os
+
+# Disable global XLA JIT unless user explicitly opted in before import.
+_os.environ.setdefault("TF_XLA_FLAGS", "--tf_xla_auto_jit=0")
 
